043a486e53aa6fc9dd03a1f95c12f33d901e7a1a
Jenn Lao <jenn.p.lao@gmail.com>
2019-01-17 09:08:56 -0700

    adds ids to buttons and creates enabling and disabling functions

--- number_guesser.js
+++ number_guesser.js
@@ -1,5 +1,11 @@
+function randomNumber(){
+  return Math.floor(Math.random() * 100) + 1;
+}
+
 function submitGuess(){
   var guess = document.getElementById("guess").value;
+  enableButton('clear');
+  enableButton('reset');
   if (guess > answer) {
     var feedback = "That is too high";
   } else if (guess < answer){
@@ -10,21 +16,30 @@ function submitGuess(){
   document.getElementById("feedback").innerHTML = 'Your Last guess was' + guess + feedback;
 }
 
-function randomNumber(){
-  return Math.floor(Math.random() * 100) + 1;
-}
 
 function clearInput(){
+  disableButton("clear");
   document.getElementById("guess").value = "";
 }
 
 function resetFeedback(){
+  disableButton("reset")
   document.getElementById("feedback").innerHTML = "";
+}
+
+function disableButton(button){
+  document.getElementById(button).disabled = true;
+}
+
+function enableButton(button){
+  document.getElementById(button).disabled = false;
 }
 
 function resetGame(){
   clearInput();
+  disableButton('clear');
   resetFeedback();
+  disableButton('reset');
   answer = randomNumber();
 }
 
